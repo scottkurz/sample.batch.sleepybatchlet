9ee9da234f08def5fd9b045eff30421f94c35930
Scott Kurz <skurz@us.ibm.com>
2022-03-09 09:13:05 -0500

    Add derby persistence to show REST API
    Signed-off-by: Scott Kurz <skurz@us.ibm.com>

--- src/main/java/com/ibm/ws/jbatch/sample/sleepybatchlet/SleepyBatchlet.java
+++ src/main/java/com/ibm/ws/jbatch/sample/sleepybatchlet/SleepyBatchlet.java
@@ -83,7 +83,8 @@ public class SleepyBatchlet extends AbstractBatchlet {
         String exitStatus = "SleepyBatchlet:i=" + i + ";stopRequested=" + stopRequested;
         log("process", "exit. exitStatus: " + exitStatus);
 
-        return exitStatus;
+    	throw new RuntimeException();
+//        return exitStatus;
     }
 
     /**
